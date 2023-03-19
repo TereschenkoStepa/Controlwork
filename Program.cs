56846ec3d32bab47e67fe5bfd9e4e9ba0f69f0d8
TereschenkoStepa <tereshchenko_stepa@mail.ru>
2023-03-20 00:04:23 +0500

    third commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод массива строк с клавиатуры
+        string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
+
+
+        // Создание нового массива строк
+        string[] result = new string[array1.Length];
+        int resultIndex = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i].Length <= 3)
+            {
+                result[resultIndex] = array1[i];
+                resultIndex++;
+            }
+        }
+
+        // Уменьшение размера массива до количества реально использованных элементов
+        Array.Resize(ref result, resultIndex);
+
+        // Вывод нового массива строк на экран
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine(result[i]);
+        }
+    }
+}
